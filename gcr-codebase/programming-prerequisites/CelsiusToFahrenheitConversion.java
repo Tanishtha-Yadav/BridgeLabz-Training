b4dfa5ca194f46cbbb6a86187d8be616045ca9fb
Tanishtha Yadav <tanishthayadav12@gmail.com>
2025-12-16 18:43:28 +0530

    [Tanishtha Yadav] : Added temperature conversion program

---
+++ gcr-codebase/programming-prerequisites/CelsiusToFahrenheitConversion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class CelsiusToFahrenheitConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature: ");
+        double celsius = sc.nextDouble();
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Temperature in Fahrenheit = " + fahrenheit);
+    }
+}
