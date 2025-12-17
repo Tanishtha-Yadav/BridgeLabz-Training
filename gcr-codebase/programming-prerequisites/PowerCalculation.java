b1977387645ecc21c4f567c662fb0fa39c357d09
Tanishtha Yadav <tanishthayadav12@gmail.com>
2025-12-17 10:52:38 +0530

    [Tanishtha Yadav] : Added Power Calculation Program

---
+++ gcr-codebase/programming-prerequisites/PowerCalculation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class PowerCalculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double base = scanner.nextDouble();
+        double exponent = scanner.nextDouble();
+
+        double result = Math.pow(base, exponent);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
