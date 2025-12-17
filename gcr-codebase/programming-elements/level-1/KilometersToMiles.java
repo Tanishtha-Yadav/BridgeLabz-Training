689d5c2b0025dbc190080de81ed4cdf45dd1c1b6
Tanishtha Yadav <tanishthayadav12@gmail.com>
2025-12-17 15:59:48 +0530

    [Tanishtha Yadav] : Added Kilometers to miles conversion program

---
+++ gcr-codebase/programming-elements/level-1/KilometersToMiles.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class KilometersToMiles{
+	public static void main(String[]args){
+		double kilometer = 10.8;  
+		
+		//create a variable to convert in miles
+		double miles = kilometer*1.6;
+		
+		// print the result
+		System.out.println("The distance "+kilometer+" km in miles is "+miles);
+	}
+}
