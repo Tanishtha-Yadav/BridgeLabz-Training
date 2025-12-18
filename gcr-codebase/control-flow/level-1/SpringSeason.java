b0bd49ce09e927435a298f6abf116d18dd4ffdda
Tanishtha Yadav <tanishthayadav12@gmail.com>
2025-12-18 14:42:45 +0530

    [Tanishtha Yadav] : Added Spring Season Program

---
+++ gcr-codebase/control-flow/level-1/SpringSeason.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class SpringSeason{
+	public static void main(String[]args){
+		// Scanner class object for user input
+		Scanner sc = new Scanner(System.in);
+		
+		// User input
+		System.out.println("Enter the number of month : ");
+		int month = sc.nextInt();
+		System.out.println("Enter day : ");
+		int day = sc.nextInt();
+		
+		// Condition check
+		if(month==3 && (day>=20 && day<=31) || 	month==4 && (day>=1 && day<=30) ||
+			month==5 && (day>=1 && day<=31) || month==6 && (day>=1 && day<=20))
+				{
+					System.out.println("Its a spring season");
+				}
+		else
+		{
+			System.out.println("Not a Spring Season");
+		}	
+
+		sc.close();
+	}
+}
