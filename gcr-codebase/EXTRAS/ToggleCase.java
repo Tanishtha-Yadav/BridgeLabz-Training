6b2cafb039975f8e0b87624a9ca3f9cbf86ba23a
Tanishtha Yadav <tanishthayadav12@gmail.com>
2025-12-23 21:36:58 +0530

    [Tanishtha Yadav] : Added toggle case program

---
+++ gcr-codebase/EXTRAS/ToggleCase.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class ToggleCase 
+{
+    // method to toggle case
+    static String toggleCase(String str)
+	{
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) 
+		{
+            char ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch))
+			{
+                result += Character.toLowerCase(ch);
+            } 
+			else if (Character.isLowerCase(ch))
+			{
+                result += Character.toUpperCase(ch);
+            } 
+			else
+			{
+                result += ch;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Toggled String: " + toggleCase(str));
+    }
+}
