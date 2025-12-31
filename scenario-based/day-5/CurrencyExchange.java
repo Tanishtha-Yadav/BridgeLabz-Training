01656996dbbddaca685e5773c6b50419df46aefa
Tanishtha Yadav <tanishthayadav12@gmail.com>
2025-12-31 23:27:24 +0530

    [Tanishtha Yadav] : Added kiosk currency exchange program

---
+++ scenario-based/day-5/CurrencyExchange.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class CurrencyExchange
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        char choice;
+
+        do
+        {
+            System.out.print("Enter amount in INR: ");
+            double inr = sc.nextDouble();
+
+            System.out.println("Choose currency:");
+            System.out.println("1. USD");
+            System.out.println("2. EUR");
+            System.out.println("3. GBP");
+
+            int option = sc.nextInt();
+            double result = 0;
+
+            switch(option)
+            {
+                case 1: result = inr * 0.012; break;
+                case 2: result = inr * 0.011; break;
+                case 3: result = inr * 0.0095; break;
+                default:
+                    System.out.println("Invalid currency");
+            }
+
+            System.out.println("Converted amount: " + result);
+
+            System.out.print("Do you want another conversion? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while(choice == 'y' || choice == 'Y');
+    }
+}
