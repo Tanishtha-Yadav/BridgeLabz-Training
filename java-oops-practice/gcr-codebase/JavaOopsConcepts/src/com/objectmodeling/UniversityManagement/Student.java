fbc4b8630a74c290fafc2df6057bbe01551e98d2
Tanishtha Yadav <tanishthayadav12@gmail.com>
2026-01-02 15:23:12 +0530

    [Tanishtha Yadav] : Added university management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/UniversityManagement/Student.java
@@ -0,0 +1,27 @@
+package com.objectmodeling.UniversityManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    private int studentId;
+    private String name;
+    private List<Course> enrolledCourses = new ArrayList<>();
+
+    public Student(int studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
+
+    // Association: Student ↔ Course
+    public void enrollCourse(Course course) {
+        enrolledCourses.add(course);
+        course.addStudent(this);
+        System.out.println(name + " enrolled in " + course.getCourseName());
+    }
+
+    public String getName() {
+        return name;
+    }
+}
