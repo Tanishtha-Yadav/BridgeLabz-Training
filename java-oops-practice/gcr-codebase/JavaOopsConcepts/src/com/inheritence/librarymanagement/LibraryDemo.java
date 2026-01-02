6423e325d5bb99cbf58544364aa20b6d53480f03
Tanishtha Yadav <tanishthayadav12@gmail.com>
2026-01-02 16:02:25 +0530

    [Tanishtha Yadav] : Added Book and Author program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/librarymanagement/LibraryDemo.java
@@ -0,0 +1,8 @@
+package com.inheritence.librarymanagement;
+
+public class LibraryDemo {
+    public static void main(String[] args) {
+        Author a = new Author("Effective Java",2018,"Joshua Bloch","Java");
+        a.displayInfo();
+    }
+}
