8914bf01bf17c13b2afba04ebfdc935b338a36ce
Tanishtha Yadav <tanishthayadav12@gmail.com>
2026-01-07 23:36:33 +0530

    [Tanishtha Yadav] : Added linear binary challenge program

---
+++ java-dsa-practice/gcr-codebase/DSAConcepts/src/com/binarysearch/LinearBinaryChallenge.java
@@ -0,0 +1,55 @@
+package com.binarysearch;
+
+import java.util.Arrays;
+
+public class LinearBinaryChallenge {
+
+    // Linear Search: First Missing Positive
+    static int firstMissingPositive(int[] arr) {
+        int n = arr.length;
+        boolean[] visited = new boolean[n + 1];
+
+        for (int num : arr) {
+            if (num > 0 && num <= n) {
+                visited[num] = true;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                return i;
+            }
+        }
+        return n + 1;
+    }
+
+    // Binary Search: Find Target Index
+    static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -1, 1};
+        int target = 4;
+
+        System.out.println("First Missing Positive: " +
+                firstMissingPositive(arr));
+
+        Arrays.sort(arr);
+        System.out.println("Target Index: " +
+                binarySearch(arr, target));
+    }
+}
+
