7cd0acb678bef47bf50b5fc369af09b91e3f7c07
Tanishtha Yadav <tanishthayadav12@gmail.com>
2026-01-24 15:03:14 +0530

    [Tanishtha Yadav] : Added Reverse Queue program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionPractice/src/com/collections/queue/ReverseQueue.java
@@ -0,0 +1,29 @@
+package com.collections.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class ReverseQueue {
+	private static void reverseQueue(Queue<Integer> q) {
+		if(q.size() == 1) return;
+		
+		int ele = q.remove();
+		reverseQueue(q);
+		q.add(ele);
+	}
+	
+	public static void main(String[] args) {
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		q.add(5);
+		
+		System.out.print("Before reverse : ");
+		System.out.println(q);
+		reverseQueue(q);
+		System.out.print("After reverse : ");
+		System.out.println(q);
+	}
+}
