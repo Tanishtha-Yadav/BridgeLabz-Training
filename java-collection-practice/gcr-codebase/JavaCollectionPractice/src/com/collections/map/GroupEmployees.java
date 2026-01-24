3bb746caedfa83c091ba48acad99e33d8c7ce4ac
Tanishtha Yadav <tanishthayadav12@gmail.com>
2026-01-24 15:16:59 +0530

    [Tanishtha Yadav] : Added Group Employees Program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionPractice/src/com/collections/map/GroupEmployees.java
@@ -0,0 +1,36 @@
+package com.collections.map;
+
+import java.util.*;
+
+public class GroupEmployees {
+	public static void main(String[] args) {
+		ArrayList<Employee> list = new ArrayList<>();
+		list.add(new Employee("Alice", "HR"));
+		list.add(new Employee("Bob", "HR"));
+		list.add(new Employee("Carlo", "IT"));
+		list.add(new Employee("Vicotr", "MR"));
+		
+		HashMap<String, List<Employee>> mp = new HashMap<>();
+		
+		for(Employee employee : list) {
+			String dept = employee.department;
+			if(!mp.containsKey(dept)) mp.put(dept, new ArrayList<Employee>());
+			mp.get(dept).add(employee);
+		}
+		System.out.println(mp);
+	}
+}
+
+class Employee {
+	String name;
+	String department;
+	
+	public Employee(String name, String department) {
+		this.name = name;
+		this.department = department;
+	}
+	
+	public String toString() {
+		return name + " " + department;
+	}
+}
