87e05d238933fcbf7d9fa4e89f692ace974e259b
Tanishtha Yadav <tanishthayadav12@gmail.com>
2026-01-26 12:54:55 +0530

    [Tanishtha Yadav] : Updated Package name program

--- java-collection-practice/gcr-codebase/JavaCollectionPractice/src/com/regex/ValidateUsername.java
+++ java-collection-practice/gcr-codebase/JavaCollectionPractice/src/com/regex/ValidateUsername.java
@@ -1,5 +1,26 @@
 package com.regex;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ValidateUsername {
 
+    public static boolean isValidUsername(String username) {
+        String regex = "^[A-Za-z][A-Za-z0-9_]{4,14}$";
+        return Pattern.matches(regex, username);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        if (isValidUsername(username)) {
+            System.out.println("Valid Username");
+        } else {
+            System.out.println("Invalid Username");
+        }
+
+        sc.close();
+    }
 }
